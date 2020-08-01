1b30b452a7ab7b2273b48264578be87905d040e5
Ctsu27 <kehuang42student@gmail.com>
2020-08-01 02:08:39 +0200

    fix-back-server -> fix sendFile path

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require("express");
-const path = require("path");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 const directory = __dirname + "/dist/harem";
 
 app.use(express.static(directory));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(directory));
+app.get("/*", (req, res) => {
+  res.sendFile("index.html", { root: directory });
 });
 
 app.listen(PORT);
